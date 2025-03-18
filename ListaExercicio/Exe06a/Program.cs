67cadf3cb3268f0e80da7e932fc4043acd39dd80
Diogo Steinke Deconto <diogo.deconto@pf.up.edu.br>
2025-03-18 11:17:28 -0300

    Projeto da Lista de Exercícios

---
+++ ListaExercicio/Exe06a/Program.cs
@@ -0,0 +1,42 @@
+﻿//2 - Criar um loop para percorrer todo o vetor
+//3 - Preencher cada posição com um valor aleatório
+//4 - Imprimir o vetor na tela
+
+int[] vetor = new int[100];
+Random r = new Random();
+
+//Criar um vetor de 100 posições inteiras
+for (int i = 0; i < vetor.Length; i++)
+{
+    vetor[i] = r.Next(100);
+}
+
+//Imprimir o vetor com os valores aleatórios
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
+
+bool troca = false;
+do{
+    troca = false;
+    for (int i = 0; i < vetor.Length - 1; i++)
+    {
+        int atual = i;
+        int prox = i + 1;
+        if (vetor[atual] > vetor[prox])
+        {
+            int aux = vetor[atual];
+            vetor[atual] = vetor[prox];
+            vetor[prox] = aux;
+            troca = true;
+        }
+    }
+}while(troca == true);
+
+//Imprimir o vetor com os valores ordenados
+Console.WriteLine("\n");
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
---
+++ ListaExercicio/Exe06b/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exe06b;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] vetor = new int[100];
+        Random r = new Random();
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = r.Next(100);
+        }
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write(vetor[i] + " ");
+        }
+
+        Array.Sort(vetor);
+
+        Console.WriteLine("\n");
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write(vetor[i] + " ");
+        }
+    }
+}
